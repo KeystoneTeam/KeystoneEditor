2a5fe905917eb632687b55e2844aa0f6333b1b97
KeystoneTeam <keystoneeditorteam@gmail.com>
2022-06-22 08:59:18 -0700

    Improved Smart UI Scaling
    * Smart UI scaling now uses a ModifyVariable mixin rather than an Overwrite for improved stability

--- src/main/java/keystone/core/mixins/WindowMixin.java
+++ src/main/java/keystone/core/mixins/WindowMixin.java
@@ -3,34 +3,19 @@ package keystone.core.mixins;
 import keystone.api.Keystone;
 import net.minecraft.client.util.Window;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(Window.class)
 public class WindowMixin
 {
-    @Shadow private double scaleFactor;
-    @Shadow private int scaledWidth;
-    @Shadow private int scaledHeight;
-    @Shadow private int framebufferWidth;
     @Shadow private int framebufferHeight;
 
-    /**
-     * @author CodeCracked
-     * @reason Implementing Smart GUI Scale in Keystone mode
-     */
-    @Overwrite
-    public void setScaleFactor(double scaleFactor)
+    @ModifyVariable(method = "setScaleFactor", at = @At("HEAD"), argsOnly = true, ordinal = 0)
+    private double smartScaleFactor(double initialValue)
     {
-        if (Keystone.isActive())
-        {
-            scaleFactor = Math.max(1, Math.round(this.framebufferHeight / 720.0));
-        }
-
-        this.scaleFactor = scaleFactor;
-        int i = (int)((double)this.framebufferWidth / scaleFactor);
-        this.scaledWidth = (double)this.framebufferWidth / scaleFactor > (double)i ? i + 1 : i;
-        int j = (int)((double)this.framebufferHeight / scaleFactor);
-        this.scaledHeight = (double)this.framebufferHeight / scaleFactor > (double)j ? j + 1 : j;
+        if (Keystone.isActive()) return Math.max(1, Math.round(this.framebufferHeight / 720.0));
+        else return initialValue;
     }
 }
